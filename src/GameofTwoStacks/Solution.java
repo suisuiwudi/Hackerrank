f1fda8193da801c6d8d64771038781aac66101d6
suisuiwudi <suisuiwudi@gmail.com>
2017-02-18 17:34:01 -0800

    GameofTwoStacks, WA, 14/30

--- src/GameofTwoStacks/Solution.java
+++ src/GameofTwoStacks/Solution.java
@@ -1,5 +1,58 @@
 package GameofTwoStacks;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int g = in.nextInt();
+		for (int a0 = 0; a0 < g; a0++) {
+			int n = in.nextInt();
+			int m = in.nextInt();
+			int x = in.nextInt();
+			int[] a = new int[n];
+			for (int a_i = 0; a_i < n; a_i++) {
+				a[a_i] = in.nextInt();
+			}
+			int[] b = new int[m];
+			for (int b_i = 0; b_i < m; b_i++) {
+				b[b_i] = in.nextInt();
+			}
+			// your code goes here
+			int[] suma = new int[n + 1];
+			suma[0] = 0;
+			for (int i = 0; i < n; i++) {
+				suma[i + 1] = suma[i] + a[i];
+			}
+			int[] sumb = new int[m + 1];
+			sumb[0] = 0;
+			for (int i = 0; i < m; i++) {
+				sumb[i + 1] = sumb[i] + b[i];
+			}
+			int lowNums = 0;
+			int upNums = n + m;
+			while (lowNums < upNums) {
+				int middle = (lowNums + upNums) / 2;
+				boolean flag = false;
+				for (int i = Math.max(0, middle - m); i <= Math.min(n, middle); i++) {
+					if (suma[i] + sumb[middle - i] <= x) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					lowNums = middle + 1;
+				} else {
+					upNums = middle;
+				}
+			}
+			System.out.println(lowNums - 1);
+		}
+	}
 
 }
